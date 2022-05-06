43d1e1699c16735fce76770e36c5c045ca40a172
Yorman Colina <yorman.colina1997@gmail.com>
2022-05-06 17:12:47 -0500

    arreglo de  detalle de la celda de descripcion

--- Jobly/Modules/JobDetail/Views/DetailCells/DescriptionViewCell.swift
+++ Jobly/Modules/JobDetail/Views/DetailCells/DescriptionViewCell.swift
@@ -20,10 +20,17 @@ class DescriptionViewCell: UICollectionViewCell {
     }
     
     func setupDescription(description: Description) {
+        let myAtribbuted = NSAttributedString(string: description.location)
+        let myString = NSMutableAttributedString("Location:  ")
+        let myRange = NSRange(location: 11, length: description.location.count)
+        myString.append(myAtribbuted)
+        myString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.blue, range: myRange)
+        
+        locationLabel.attributedText = myString
         descriptionLabel.numberOfLines = 0
         descriptionLabel.text = "Description: " + description.description
-        salaryLabel.text = "Salary: " + description.salary
-        locationLabel.text = "Location: " + description.location
+        salaryLabel.text = "Salary:  " + description.salary
+//        locationLabel.text = "Location: " + description.location
         
     }
 
